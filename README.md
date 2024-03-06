2fd6c6b5ea8052af6b352e7a3033d349a50cffe9
Karina534 <smirnova7.04@mail.ru>
2024-03-06 16:47:15 +0300

    added app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # programming_project
+## HTML страницы нужно класть в itmo_hh/templates/itmo_hh
+## CSS страницы нужно класть в itmo_hh/static/itmo_hh
---
+++ program_hh/itmo_hh/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ItmoHhConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "itmo_hh"
