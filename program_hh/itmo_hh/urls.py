f0e13d3a511155072c4538cee76c4c1ff2e5065b
Karina534 <smirnova7.04@mail.ru>
2024-03-09 13:45:35 +0300

    fixed directories for html and css

--- program_hh/itmo_hh/urls.py
+++ program_hh/itmo_hh/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+
 from .views import *
 
 urlpatterns = [
     path("", index),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- program_hh/itmo_hh/views.py
+++ program_hh/itmo_hh/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("page")
+    return render(request, 'itmo_hh/index.html')
