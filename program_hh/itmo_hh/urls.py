0e9d7d70bf819ba84841fe1306d4b176fcf53a86
Karina534 <smirnova7.04@mail.ru>
2024-03-18 20:21:10 +0300

    added html and css pages

--- program_hh/itmo_hh/urls.py
+++ program_hh/itmo_hh/urls.py
@@ -5,5 +5,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", index),
+    path("", index, name='home'),
+    path("personal_account", personal_account, name='personal_account'),
+    path('registration', registration, name='registration'),
+    path('startapp', startapp, name='startapp'),
+    path('project', project, name='project'),
+    path('summary', summary, name='summary'),
+    path('resume_project', resume_project, name='resume_project'),
+    path('resume_person', resume_person, name='resume_person')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- program_hh/itmo_hh/views.py
+++ program_hh/itmo_hh/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'itmo_hh/index.html')
+
+def personal_account(request):
+    return render(request, 'itmo_hh/personal_account.html')
+
+def registration(request):
+    return render(request, 'itmo_hh/registration.html')
+
+def startapp(request):
+    return render(request, 'itmo_hh/startapp.html')
+
+def project(request):
+    return render(request, 'itmo_hh/project.html')
+
+def summary(request):
+    return render(request, 'itmo_hh/summary.html')
+
+def resume_project(request):
+    return render(request, 'itmo_hh/resume_project.html')
+
+def resume_person(request):
+    return render(request, 'itmo_hh/resume_person.html')
