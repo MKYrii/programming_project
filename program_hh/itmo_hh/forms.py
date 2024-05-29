0831bbe2ec87fd59997e79dcfd1a73fd4e7f2e80
Karina534 <smirnova7.04@mail.ru>
2024-05-29 13:01:45 +0300

    made resumes and projects

--- program_hh/itmo_hh/forms.py
+++ program_hh/itmo_hh/forms.py
@@ -2,35 +2,6 @@ from django import forms
 from .models import *
 from django.contrib.auth.models import User
 
-'''class AddResumePerson(forms.Form):
-    experience_choice = (
-        ('no_experience', 'Нет опыта'),
-        ('1-3', '1-3 года'),
-        ('3-5', '3-5 лет'),
-        ("5", "5+")
-    )
-    education_level_choice = (
-        ('bakalavriat', 'Бакалавриат'),
-        ('magistr', 'Магистратура'),
-        ('aspirantyra', 'Аспирантура')
-    )
-    user_id = forms.ModelChoiceField(queryset=User.objects.all())
-    title = forms.CharField(max_length=255, label='Заголовок резюме')
-    FIO = forms.CharField(max_length=255, label='ФИО')
-    sex = forms.MultipleChoiceField(label='Пол', choices=(('men', 'Мужчина'), ('women', 'Женщина')),
-                                    widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-inside'}))
-
-    birthday = forms.DateField(label='День рождения',
-                            input_formats=['%d/%m/%Y', '%d/%m/%y', '%d.%m.%Y', '%d.%m.%y'])
-
-    napravlenie = forms.ModelChoiceField(queryset=Napravlenie.objects.all())
-    education_level = forms.ChoiceField(choices=education_level_choice, label='Уроень образования')
-    experience = forms.ChoiceField(choices=experience_choice, label='Опыт')
-    sphere = forms.ModelChoiceField(queryset=Sphere.objects.all())
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Описание')
-    file = forms.FileField(required=False, label='Прикрепите файлы')'''
-
-
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
