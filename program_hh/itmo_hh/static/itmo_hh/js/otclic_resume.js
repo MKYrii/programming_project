f2f2a6877f8d9ce33e19dc6873fb3629ace3bac6
Uriy <mihailovywork@gmail.com>
2024-06-01 01:28:45 +0300

    косметические правки + новые проекты

--- program_hh/itmo_hh/static/itmo_hh/js/otclic_resume.js
+++ program_hh/itmo_hh/static/itmo_hh/js/otclic_resume.js
@@ -1,4 +1,22 @@
 
+
+
+search_btn = document.getElementsByName("search_btn");
+
 search_btn.style.width = 100 / resumes_length + '%';
 
-back.style.background = '#ff0000';
+otklik_btn.onclick = async function() {
+        for(let i = 0; i < 50; i++){
+            await new Promise(resolve => setTimeout(resolve, 5));
+            data_frame.style.width = 200 + i * 5;
+            data_frame.style.height = 40 + i * 5;
+        }
+        
+        }
+        s_btn.style.left = 1000;
+        data_frame.style.height = 1000;
+
+     
+        search_btn.style.width = 100 / resumes_length + '%';
+    
+    alert('hello');
